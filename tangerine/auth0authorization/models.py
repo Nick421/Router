ebfa9dd9f13db267b8927eed0abccfe18e578b1a
Niravit Theng <nthe0838@uni.sydney.edu.au>
2019-09-27 13:47:22 +1000

    untrack caches files and set up database

--- tangerine/auth0authorization/models.py
+++ tangerine/auth0authorization/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
 
-# Create your models here.
+
+class RemoteUserManager(BaseUserManager):
+    def create_user(self, subject, password):
+        if not subject:
+            raise ValueError('Users must have a subject')
+        user = self.model(
+            subject=subject
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, subjet):
+        user = self.create_user(subject=subject)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    subject = models.CharField(max_length=35, unique=True, primary_key=True)
+
+    objects = RemoteUserManager()
+    USERNAME_FIELD = 'subject'
+    REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
