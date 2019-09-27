6c8e5b073cc3a044518e236919c93af3c7637cc0
Niravit Theng <nthe0838@uni.sydney.edu.au>
2019-09-27 23:57:20 +1000

    Adding in skeleton for history API endpoint, can be tested from the front end Reformat how the subject is parse and store in database Renamed URLs and add more for each API endpoint Added basic serializers for history JSON

--- tangerine/auth0authorization/user.py
+++ tangerine/auth0authorization/user.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate
 
+
 def jwt_get_username_from_payload_handler(payload):
-    username = payload.get('sub').replace('|', '.')
+    username = payload.get('sub')
     authenticate(remote_user=username)
     return username
--- tangerine/auth0authorization/views.py
+++ tangerine/auth0authorization/views.py
@@ -4,10 +4,17 @@ import json
 from functools import wraps
 
 from django.http import JsonResponse
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import APIException
 from six.moves.urllib import request as req
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.hazmat.backends import default_backend
+
+
+from .models import User
+from .serializers import HistorySerializers
 
 
 def get_token_auth_header(request):
@@ -54,19 +61,78 @@ def public(request):
     )
 
 
-@api_view(["GET"])
-def private(request):
+@api_view(["POST"])
+def login(request):
     return JsonResponse(
         {
-            "message": "Hello from a private endpoint! You need to be authenticated to see this."
+            "message": "Authenticated and logged in."
         }
     )
 
 
-@api_view(["GET"])
-@requires_scope("read:messages")
-def private_scoped(request):
+"""
+This method will parse the token to get subject to store as user_id
+Then it will query the database for history table using user_id as foreign key
+
+@GET 
+return an array of JSON of previous route
+
+@POST
+store the route 
+"""
+
+
+@api_view(["GET", "POST"])
+@parser_classes([JSONParser])
+# @requires_scope("read:history")
+def private_history(request):
+    # Decode the token
+    token = get_token_auth_header(request)
+    decoded = jwt.decode(token, None, None)
+    # get subject part of the token as our pk
+    user_id = decoded.get('sub')
+    # this fetch the data associated with user in table auth0authorization_user with same pk
+    print(User.objects.get(subject=user_id))
+
+    """
+    Currently dummy response and data will add proper database interaction 
+    """
+    # TODO
+    if request.method == 'POST':
+         # validating the JSON before adding in to the database
+        valid_data = HistorySerializers(data=request.data)
+        if valid_data.is_valid():
+            # TODO add to database
+            post_data = valid_data.validated_data
+        else:
+            # print(valid_data.errors)
+            raise APIException(valid_data.errors)
+        return JsonResponse({'received data': request.data})
+
+    return JsonResponse({
+        'Route 1': {
+            "Source": "UTS",
+            "Destination": "Central station",
+            "Keyword": "food"
+        },
+        'Route 2': {
+            "Source": "USYD",
+            "Destination": "Townhall station",
+            "Keyword": "bar"
+        },
+        'Route 3': {
+            "Source": "Campsie station",
+            "Destination": "Canterbury station",
+            "Keyword": "gym"
+        }
+    })
+
+# TODO
+
+
+@api_view(["GET", "POST"])
+# @requires_scope("read:favourite")
+def private_favourite(request):
     return JsonResponse(
         "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
     )
-
