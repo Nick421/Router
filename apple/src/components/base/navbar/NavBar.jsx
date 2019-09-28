64282226558027e08446999a848c0d462c9cc973
Dat Nguyen <dngu4013@uni.sydney.edu.au>
2019-09-29 00:12:20 +1000

    refractor navbar back to jsx class to be able to receive props if needed

--- apple/src/components/base/navbar/NavBar.jsx
+++ apple/src/components/base/navbar/NavBar.jsx
@@ -4,20 +4,20 @@ import { IconNames } from "@blueprintjs/icons"
 
 import * as auth from "../../../services/auth0/auth0";
 
-const NavBar = () => {
-    const navbarStyle = {
-        backgroundColor: Colors.DARK_GRAY1
-        };
-    return (
-        <Navbar className={Classes.DARK} style={navbarStyle}>
-            <Navbar.Group align={Alignment.LEFT}>
-                <Navbar.Heading className="text-white-100">Tangerine</Navbar.Heading>
-            </Navbar.Group>
-            <Navbar.Group align={Alignment.RIGHT}>
-            <Button className={Classes.MINIMAL} icon={IconNames.LOG_OUT} onClick={auth.userLogout}/>
-            </Navbar.Group>
-        </Navbar>
-    )
+export default class NavBar extends React.PureComponent {
+    render() {
+        const navbarStyle = {
+            backgroundColor: Colors.DARK_GRAY1
+            };
+        return (
+            <Navbar className={`${Classes.DARK} w-screen`} style={navbarStyle}>
+                <Navbar.Group align={Alignment.LEFT}>
+                    <Navbar.Heading className="text-white-100">Tangerine</Navbar.Heading>
+                </Navbar.Group>
+                <Navbar.Group align={Alignment.RIGHT}>
+                <Button className={Classes.MINIMAL} icon={IconNames.LOG_OUT} onClick={auth.userLogout}/>
+                </Navbar.Group>
+            </Navbar>
+        )
+    }
 }
-
-export default NavBar;
