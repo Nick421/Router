3bcac90110c33fd1aeda1fc4e4a8c3f9ced7b436
Joo Hwan Kwon <jkwo8485@uni.sydney.edu.au>
2019-10-13 16:28:52 +1100

    Update FavouritesPopUp.jsx

--- apple/src/components/Favourites/FavouritesPopUp.jsx
+++ apple/src/components/Favourites/FavouritesPopUp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export class HistoryPopUp extends React.Component {
+export class FavouritesPopUp extends React.Component {
   
   constructor(props){
     super (props);
