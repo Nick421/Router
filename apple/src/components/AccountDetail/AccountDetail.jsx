648c7e9b99b28ca41fa172fc731b6e0935b44fa7
JooHwan <krandy123@gmail.com>
2019-10-14 15:11:06 +1100

    Favourites and History

---
+++ apple/src/components/AccountDetail/AccountDetail.jsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+
+import { Nav, Button, Drawer, Classes } from "@blueprintjs/core";
+import BaseLayout from "./../base/BaseLayout";
+
+export default class History extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div>
+        <div
+              className="bg-cover bg-center shadow overflow-hidden h-100 w-40 text-center item-center"
+              style={{
+                backgroundColor: "white",
+                position: "fixed",
+                left: "30%",
+                top: "6.5rem",
+                height: "30rem",
+                width: "40%",
+                borderColor: "black",
+                borderWidth: "0.1rem",
+                opacity: "1"
+              }}
+            > 
+              <h1
+                className="block p-10 text-xl text-grey-darker text-center font-bold border-purple hover:bg-grey-lighter border-r-4"
+                style={{
+                  fontSize: "2.5rem",
+                  fontFamily: "alegreya",
+                  color: "black",
+                  textDecoration: "none"
+                }}
+              >
+                {" "}
+                Account Detail{" "}
+              </h1>
+              
+              <h2
+                className="block p-10 text-xl text-grey-darker text-left font-bold border-purple hover:bg-grey-lighter border-r-4"
+                style={{
+                  fontSize: "1.4rem",
+                  fontFamily: "helvetica",
+                  color: "black",
+                  textDecoration: "none",
+                  padding: "1rem"
+                }}
+              >
+                {" "}
+                Name: John Doe{" "}
+              </h2>
+
+              <h2
+                className="block p-10 text-xl text-grey-darker text-left font-bold border-purple hover:bg-grey-lighter border-r-4"
+                style={{
+                  fontSize: "1.4rem",
+                  fontFamily: "alegreya",
+                  color: "black",
+                  textDecoration: "none",
+                  padding: "1rem"
+                }}
+              >
+                {" "}
+                Number of Trips made: 92{" "}
+              </h2>
+
+              <h2
+                className="block p-10 text-xl text-grey-darker text-left font-bold border-purple hover:bg-grey-lighter border-r-4"
+                style={{
+                  fontSize: "1.4rem",
+                  fontFamily: "alegreya",
+                  color: "black",
+                  textDecoration: "none",
+                  padding: "1rem"
+                }}
+              >
+                {" "}
+                Number of Favourite routes: 15{" "}
+              </h2>
+
+              <h2
+                className="block p-10 text-xl text-grey-darker text-left font-bold border-purple hover:bg-grey-lighter border-r-4"
+                style={{
+                  fontSize: "1.4rem",
+                  fontFamily: "alegreya",
+                  color: "black",
+                  textDecoration: "none",
+                  padding: "1rem"
+                }}
+              >
+                {" "}
+                Gmail account: blahblahblah@gmail.com{" "}
+              </h2> 
+
+
+            </div>
+        </div>
+      </div>
+    );
+  }
+
+  returnName(trip) {
+    return trip.name;
+  }
+
+  renderCollectionList() {
+    var testdata = this.createTestData();
+    testdata.maps.map(trip => <li>{trip.name}</li>);
+  }
+}
--- apple/src/components/Sidebar/SideBar.jsx
+++ apple/src/components/Sidebar/SideBar.jsx
@@ -1,56 +1,100 @@
 import * as React from "react";
 
-import{ Nav, Button,Drawer, Classes, Menu, MenuItem} from "@blueprintjs/core";
+import{ Nav, Button, Classes, Menu, MenuItem} from "@blueprintjs/core";
 import BaseLayout from "./../base/BaseLayout";
 import Favourites from "./../Favourites/Favourites";
+import History from "./../history/History";
+import AccountDetail from "./../AccountDetail/AccountDetail";
 
 export default class SideBar extends React.Component{
     constructor(props){
         super(props);
-        this.state = {isOpen: false};
-        // this.favState = {isFavouriteOpen: false};
+        this.state = {
+            isOpen: false,
+            isHistoryOpen: false,
+            isFavouriteOpen: false,
+            isAccountDetailOpen: false,
+        }
 
         this.handleClick = this.handleClick.bind(this);
-        // this.favouriteHandleClick = this.favouriteHandleClick.bind(this);
+        this.handleHistoryClick = this.handleHistoryClick.bind(this);
+        this.handleFavouriteClick = this.handleFavouriteClick.bind(this);
+        this.handleAccountDetailClick = this.handleAccountDetailClick.bind(this);
+        this.handleHistoryClose = this.handleHistoryClose.bind(this);
     }
 
     handleClick(){
         this.setState(state => ({isOpen: !state.isOpen}));
     }
 
-    // favouriteHandleClick(){
-    //     this.setState(favState => ({isFavouriteOpen : !favState.isFavouriteOpen}));
-    // }
+    handleHistoryClick(){
+        this.setState(state => ({isHistoryOpen: !state.isHistoryOpen}));
+    }
+
+    handleFavouriteClick(){
+        this.setState(state => ({isFavouriteOpen: !state.isFavouriteOpen}));
+    }
+
+    handleAccountDetailClick(){
+        this.setState(state => ({isAccountDetailOpen: !state.isAccountDetailOpen}));
+    }
+
+    handleHistoryClose(){
+        this.setState({isHistoryOpen: false});
+    }
 
     render(){
         return(
              <div>
-                 <div><Button className="bg-blue-500 hover:bg-blue-700 text-blue-600 hover:text-red-600 font-bold py-2 px-4 rounded" onClick={this.handleClick}>Menu</Button></div>
+                 <div><Button className="bg-blue-500 hover:bg-blue-700 text-blue-600 hover:text-red-600 font-bold py-2 px-4 rounded" 
+                        onClick={this.handleClick}>Menu</Button></div>
+                      
                        <div>{this.state.isOpen? 
-                            <div className="bg-cover bg-center shadow overflow-hidden h-100 w-40" style={{ backgroundColor: 'black', opacity: "0.7", position: 'absolute', left: "0rem", top: '5.5rem', height:'50rem', width: "20rem" }}>
-                                <ul className="list-reset">
-                                     <li >
-                                        <a href = "#" className="block p-10 text-xl text-grey-darker text-white font-bold border-purple hover:bg-grey-lighter border-r-4" 
-                                        style={{fontSize: "2.5rem",  fontFamily: "alegreya",color: "white", textDecoration: 
-                                            "none"}}>Home</a>
-                                     </li>
+                            <div className="bg-cover bg-center shadow overflow-hidden h-100 w-40"
+                             style={{ backgroundColor: 'black', opacity: "0.8", position: 'absolute', left: "0rem", top: '5.5rem', height:'50rem', width: "20rem" }}>
+                                              
+                                <ul >
                                      <li >
                                         <a href= "#" className="block p-10 text-xl text-grey-darker text-white font-bold border-grey-lighter hover:border-purple-light hover:bg-grey-lighter border-r-4" 
-                                            onClick={this.favouriteHandleClick}
-                                            style={{fontSize: "2.5rem", fontFamily: "alegreya", color: "white", textDecoration: 
-                                            "none"}}>Favourites</a>
-                                                                 
+                                            onClick={this.handleFavouriteClick} 
+                                            style={{fontSize: "2.5rem", fontFamily: "alegreya", color: "white", textDecoration:  "none" }}>
+                                            Favourites
+                                        </a>
+
+                                        <div>
+                                            {this.state.isFavouriteOpen? <div><Favourites/></div> : null}
+                                        </div>
+                  
                                      </li>
+
                                      <li >
-                                     <a href="#" className="block p-10 text-xl text-grey-darker text-white font-bold border-grey-lighter hover:border-purple-light hover:bg-grey-lighter border-r-4" 
-                                     style={{fontSize: "2.5rem", fontFamily: "alegreya",color: "white", textDecoration: 
-                                            "none"}}>Bookmark</a>
+                                        <a href= "#" className="block p-10 text-xl text-grey-darker text-white font-bold border-grey-lighter hover:border-purple-light hover:bg-grey-lighter border-r-4" 
+                                            onClick={this.handleHistoryClick}
+                                            style={{fontSize: "2.5rem", fontFamily: "alegreya", color: "white", textDecoration:  "none" }}>
+                                            History
+                                        </a>
+
+                                        <div>
+                                            {this.state.isHistoryOpen? <div><History/> </div> : null}
+                                        </div>
+                  
                                      </li>
-                                    <li >
-                                        <a href="#" className="block p-10 text-xl text-grey-darker text-white font-bold border-grey-lighter hover:border-purple-light hover:bg-grey-lighter border-r-4" 
-                                        style={{fontSize: "2.5rem", fontFamily: "alegreya", color: "white", textDecoration: 
-                                            "none"}}>Account Detail</a>
-                                    </li>
+
+                                     <li >
+                                        <a href= "#" className="block p-10 text-xl text-grey-darker text-white font-bold border-grey-lighter hover:border-purple-light hover:bg-grey-lighter border-r-4" 
+                                            onClick={this.handleAccountDetailClick}
+                                            style={{fontSize: "2.5rem", fontFamily: "alegreya", color: "white", textDecoration:  "none" }}>
+                                            Account Detail
+                                        </a>
+
+                                        <div>
+                                            {this.state.isAccountDetailOpen? <div> <AccountDetail/> </div> : null }
+                                        </div>
+                  
+                                     </li>
+                                   
+
+                                    
                                     </ul>
                                 </div>:
                                 null}
@@ -60,3 +104,5 @@ export default class SideBar extends React.Component{
     }
 
 }
+
+
