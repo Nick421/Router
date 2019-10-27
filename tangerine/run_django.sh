7e6e321f8484753baeecb463898ab984ba1b1854
Dat Nguyen <dngu4013@uni.sydney.edu.au>
2019-10-28 01:20:56 +1100

    fix dependencies for virtualenv

--- tangerine/run_django.sh
+++ tangerine/run_django.sh
@@ -3,6 +3,7 @@
 source ./dev_env/bin/activate
 echo "Installing dependencies"
 pip install --upgrade pip
+pip install https://pypi.python.org/packages/source/psycopg2/psycopg2-2.8.tar.gz
 pip install -r ./requirements.txt
 
 echo "Migrate"
