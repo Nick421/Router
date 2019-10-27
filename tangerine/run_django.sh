5b41fd4ed1c2ce2b861c68748dee4accab26feab
Dat Nguyen <dngu4013@uni.sydney.edu.au>
2019-10-28 01:27:13 +1100

    fix dependencies uri

--- tangerine/run_django.sh
+++ tangerine/run_django.sh
@@ -3,10 +3,11 @@
 source ./dev_env/bin/activate
 echo "Installing dependencies"
 pip install --upgrade pip
-pip install https://pypi.python.org/packages/source/psycopg2/psycopg2-2.8.tar.gz
+pip install https://pypi.python.org/packages/source/p/psycopg2/psycopg2-2.8.tar.gz
 pip install -r ./requirements.txt
 
 echo "Migrate"
+python manage.py makemigrations
 python manage.py migrate
 echo "Starting server"
 python manage.py runserver
