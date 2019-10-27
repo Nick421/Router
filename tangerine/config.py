e348613dd8a1e407dc516863f0f170cee86d41d1
Dat Nguyen <dngu4013@uni.sydney.edu.au>
2019-10-27 23:50:06 +1100

    fix migration issue with script

--- tangerine/config.py
+++ tangerine/config.py
@@ -1,7 +1,7 @@
 database = dict(
     name = 'elec3609',
     user = 'postgres',
-    password = '',
+    password = 'postgres',
     host = 'ec2-3-24-136-152.ap-southeast-2.compute.amazonaws.com',
     port = '5432',
 )
