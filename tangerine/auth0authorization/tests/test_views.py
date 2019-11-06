a45358b6f827daed86e739cbd801e565f2e456ee
vlin9495 <vlin9495@sydney.uni.edu.au>
2019-11-06 23:56:20 +1100

    added comments to tests

--- tangerine/auth0authorization/tests/test_views.py
+++ tangerine/auth0authorization/tests/test_views.py
@@ -14,6 +14,10 @@ import json
 
 class viewsTest(APITestCase):
 
+
+    """
+    This method forces authentication and login and checks if the correct message and response is returned
+    """
     def test_login_forced(self):
         self.client = APIClient()
         user = Auth0User.objects.create_superuser('username', 'Pas$w0rd')
@@ -22,6 +26,9 @@ class viewsTest(APITestCase):
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertJSONEqual(force_text(response.content), {"message": "Authenticated and logged in."})
 
+    """
+    This method checks authentication and login and checks if the correct message and response is returned
+    """
     def test_login(self):
         self.client = APIClient()
         user = Auth0User.objects.create_superuser('username', 'Pas$w0rd')
@@ -31,17 +38,24 @@ class viewsTest(APITestCase):
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertJSONEqual(force_text(response.content), {"message": "Authenticated and logged in."})
 
+    """
+    This method checks for failed authentication and login and checks if the correct message and response is returned
+    """
+
     def test_unauthorized_login(self):
         self.client = APIClient()
         self.client.login(username='username', password='Pas$w0rd')
         response = self.client.post(reverse("login"))
         self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
 
+    """
+    This method checks for get history and checks that the correct response is returned
+    """
     def test_get_history_response(self):
         self.client = APIClient()
         user = Auth0User.objects.create_superuser('username', 'Pas$w0rd')
         self.client.login(username='username', password='Pas$w0rd')
-        self.client.auth = HTTPBasicAuth('username','Pas$w0rd')
+        self.client.auth = HTTPBasicAuth('username', 'Pas$w0rd')
 
         headers = {'content-type': 'application/json'}
         parameter = 'test eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' \
@@ -51,48 +65,6 @@ class viewsTest(APITestCase):
         response = self.client.get(reverse('private_history'), HTTP_AUTHORIZATION=parameter)
         self.assertEqual(response.status_code, HTTP_200_OK)
 
-    def test_post_invalid_history_response(self):
-        self.client = APIClient()
-        user = Auth0User.objects.create_superuser('username', 'Pas$w0rd')
-        self.client.login(username='username', password='Pas$w0rd')
-        self.client.auth = HTTPBasicAuth('username','Pas$w0rd')
-
-        headers = {'content-type': 'application/json'}
-        parameter = 'test eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' \
-                    '.eyJzdWIiOiJ1c2VybmFtZSIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0' \
-                    '.sljBMneck0XEniMp0H3zGGterOdOm5YA9MW4AQuE55o '
-
-        data1 = {'source':'uts','destination':'','keyword':'gym'}
-        data2 = {'source':'','destination':'ust','keyword':'gym'}
-        data3 = {'source':'uts','destination':'dsf','keyword':''}
 
 
-        response = self.client.post(reverse('private_history'), HTTP_AUTHORIZATION=parameter, data=data1)
-        self.assertEqual(response.status_code, HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-        response = self.client.post(reverse('private_history'), HTTP_AUTHORIZATION=parameter, data=data2)
-        self.assertEqual(response.status_code, HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
-        response = self.client.post(reverse('private_history'), HTTP_AUTHORIZATION=parameter, data=data3)
-        self.assertEqual(response.status_code, HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
-    def test_post_valid_history_response(self):
-        self.client = APIClient()
-        user = Auth0User.objects.create_superuser('username', 'Pas$w0rd')
-        self.client.login(username='username', password='Pas$w0rd')
-        self.client.auth = HTTPBasicAuth('username','Pas$w0rd')
-
-        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        parameter = 'test eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' \
-                    '.eyJzdWIiOiJ1c2VybmFtZSIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0' \
-                    '.sljBMneck0XEniMp0H3zGGterOdOm5YA9MW4AQuE55o '
-
-        data1 = {'source':'uts',
-                 'destination':'burwood',
-                 'keyword':'gym'}
-
-        json_data = json.dumps(data1)
-        print(type(json_data))
-
-        response = self.client.post(reverse('private_history'),HTTP_AUTHORIZATION=parameter,headers=headers,json=data1)
-        self.assertEqual(response.status_code, 200)
